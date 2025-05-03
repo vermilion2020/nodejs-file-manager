2595d3ba2362cd08913d364ca07a04a9acde21b8
Militsa Tuseeva <militsa.tuseeva.ext@abtasty.com>
2025-05-03 18:49:08 +0200

    feat: add handling for hash, compress and decompress commands

--- src/commands.js
+++ src/commands.js
@@ -1,6 +1,8 @@
 import { cd, up } from './handlers/navigation.js';
 import { help, exit, os } from './handlers/technical.js';
+import { hash } from './handlers/hash.js';
 import { cat, add, makeDir, remove, ls, rn, mv, cp } from './handlers/fs/index.js';
+import { compress, decompress } from './handlers/compress.js';
 
 export const COMMANDS = {
   'up': up,
@@ -14,6 +16,9 @@ export const COMMANDS = {
   'mv' : mv,
   'rm' : remove,
   'os' : os,
+  'hash' : hash,
+  'compress' : compress,
+  'decompress' : decompress,
   'help' : help,
   '.exit' : exit,
 };
